7ff6dd91353c8b7a8da574a8189f1c342ab5e33a
Shivam Kedia <65805659+shivamkedia17@users.noreply.github.com>
2023-02-10 15:13:33 +0530

    added install scripts

---
+++ backup_configs/installs.sh
@@ -0,0 +1,28 @@
+#!/bin/bash
+
+# update all apps
+sudo apt update
+# Install tldr pages
+sudo apt install tldr
+
+# Install zsh
+sudo apt install zsh
+
+# Install oh-my-zsh
+sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
+
+# Change shell to zsh
+echo "To change your shell to zsh, run:"
+echo 'chsh -s /bin/zsh'
+
+# Install zsh custom plugins
+# Zsh autosuggestions
+git clone https://github.com/zsh-users/zsh-autosuggestions ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-autosuggestions
+# Zsh syntax highlighting
+git clone https://github.com/zsh-users/zsh-syntax-highlighting.git ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-syntax-highlighting
+
+# add plugins to .zshrc
+echo "add plugins to .zshrc,"
+vim ~/.zshrc -c "/plugins="
+
+
---
+++ backup_configs/sync-dotfiles.sh
@@ -0,0 +1,9 @@
+#/bin/zsh
+
+# My own themes
+
+git clone https://github.com/shivamkedia17/dotfiles.git
+ln -f ~/dotfiles/zsh-mythemes/*.zsh-theme ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/themes/
+ln -f ~/dotfiles/vim/.vimrc ~
+cp -r ~/dotfiles/vim/.vim ~
+
