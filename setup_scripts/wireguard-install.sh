1a539b35e83ee353ce27fac19a91568724090d9f
Shivam Kedia <65805659+shivamkedia17@users.noreply.github.com>
2023-02-11 19:09:57 +0530

    fixed wireguard directory change

--- setup_scripts/wireguard-install.sh
+++ setup_scripts/wireguard-install.sh
@@ -2,7 +2,8 @@
 
 git clone https://github.com/shivamkedia17/dotfiles.git ~
 mkdir -p ~/docker/wireguard
-cd ~/dotfiles/setup_scripts/docker-compose.yml ~/docker/wireguard
+cp ~/dotfiles/setup_scripts/docker-compose.yml ~/docker/wireguard
+cd ~/docker/wireguard
 
 sudo docker compose up
 # To show QR code: 'docker exec -it wireguard /app/show-peer 1 2 3'
