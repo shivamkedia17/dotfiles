d9a857ef9abe7d82f0a958677e549c8be942003a
shivamkedia17 <65805659+shivamkedia17@users.noreply.github.com>
2025-01-14 19:41:38 +0530

    PS: reorganise nvim config

--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -1,9 +1,76 @@
---[[ init.lua ]]
+local cmd = vim.cmd
 
--- IMPORTS
- require('vars')	--Variables vim.g
- require('opts')	--Options vim.opt
- require('keys')	--Keymaps
- require('plugins')	--Plugins vim.fn
- require('lsp_config')  --LSP Config
- require('commands')    --Commands vim.cmd
+cmd.colorscheme 'midnight'
+local api = vim.api
+local opt = { noremap=true, silent=true }
+
+api.nvim_set_keymap('n', 'gd', '<cmd>lua vim.lsp.buf.definition()<CR>', opt)
+api.nvim_set_keymap('n', 'gr', '<cmd>lua vim.lsp.buf.references()<CR>', opt)
+api.nvim_set_keymap('n', '<leader>rn', '<cmd>lua vim.lsp.buf.rename()<CR>', opt)
+
+require('mason').setup()
+require('mason-lspconfig').setup()
+
+require('lspconfig').lua_ls.setup {}
+require('lspconfig').rust_analyzer.setup {}
+require('lspconfig').denols.setup {}
+require('lspconfig').clangd.setup {}
+require('lspconfig').gopls.setup({
+    cmd = { "gopls" },
+    root_dir = function(fname)
+        return require('lspconfig').util.root_pattern("go.mod")(fname) or vim.loop.os_homedir()
+    end,
+    settings = {
+        gopls = {
+            analyses = {
+                unusedparams = true,
+                staticcheck = true,
+            },
+            staticcheck = true,
+        },
+    },
+})
+
+-- [[ opts.lua ]]
+
+local opt = vim.opt
+
+-- [[ Context ]]
+opt.number = true
+opt.relativenumber = true
+
+-- [[ Theme ]]
+opt.syntax = "ON"
+opt.termguicolors = true
+
+-- [[ Search ]]
+opt.hlsearch = false
+
+-- [[ Whitespace ]]
+opt.expandtab = true
+opt.tabstop = 4        -- Number of spaces that a <Tab> counts for
+opt.shiftwidth = 4     -- Number of spaces to use for each step of (auto)indent
+opt.smartindent = true
+
+
+opt.scrolloff=8
+-- Hi
+
+local Plug = vim.fn['plug#']
+
+vim.call('plug#begin')
+
+Plug('williamboman/mason.nvim')
+Plug('williamboman/mason-lspconfig.nvim')
+Plug('neovim/nvim-lspconfig')
+Plug('tpope/vim-surround')
+Plug('tpope/vim-commentary')
+Plug('kaarmu/typst.vim')
+Plug('dasupradyumna/midnight.nvim')
+
+vim.call('plug#end')
+--[[ vars.lua ]]
+
+local g = vim.g
+g.t_co = 256
+g.background = "dark"
--- .config/nvim/lua/keys.lua
+++
@@ -1,6 +0,0 @@
-local api = vim.api
-local opt = { noremap=true, silent=true }
-api.nvim_set_keymap('n', 'gd', '<cmd>lua vim.lsp.buf.definition()<CR>', opt)
-api.nvim_set_keymap('n', 'gr', '<cmd>lua vim.lsp.buf.references()<CR>', opt)
-api.nvim_set_keymap('n', '<leader>rn', '<cmd>lua vim.lsp.buf.rename()<CR>', opt)
-
